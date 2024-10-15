2f5734be983b9910eed7284d5729bb347b9ccba7
Evan <137832376+EvanOLP03@users.noreply.github.com>
2024-10-15 10:11:50 +0200

    Delete Job_Board directory

--- Job_Board/accueil/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home
-from . import views
-
-urlpatterns = [
-    path('', home, name='home'),  
-    path('inscription/', views.student_signup, name='student_signup'),
-    path('login/', views.login_view, name='login'),
-]
